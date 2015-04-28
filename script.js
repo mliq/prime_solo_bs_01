72756b051fb6c07478fc3f2df452ab7a724815c1
mliq <liquori@gmail.com>
2015-04-28 11:54:53 -0500

    Initial Commit, images displaying

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-var apikey = ''; // Put your API key here
-
+var apikey = 'b5749836178866bb15f4b41b15ac30c692573f21'; // Put your API key here
+var i = 0;
+var str;
+var imgURL;
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
-    console.log(results);
+    str = '';
+
+    for(i = 0;i < 9; i++){
+        console.log(results[i]);
+
+        //image
+        if(!results[i].image){
+            imgURL = "sad_face.jpg";
+        } else {
+            imgURL = results[i].image.small_url;
+        }
+
+        str += "<div class='well col-md-4'>" +
+            "<img class='hidden-sm hidden-xs' src=" + imgURL + "><br>"
+        + "</div>";
+
+    }
+    $('.results').append(str);
 }
 
 $(document).ready(function() {
 
 	// Start the search here!
-	
+	search('batman');
 });
 
 // HELPER FUNCTION
